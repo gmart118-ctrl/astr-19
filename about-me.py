678e54cdf23aaecc2a5b3aae8747f88a53e25209
gmart118-ctrl <gmart118@ucsc.edu>
2025-10-02 12:13:26 -0700

    about-me.py

--- about-me.py
+++ about-me.py
@@ -2,8 +2,7 @@
 #about my favorite movie and my favorite food
 
 #This function tell the computer to print
-#my name and two sentences about my favorite movie
-#and food
+#my name and two sentences about my favorite movie and food
 def PrintIntro():
 	print("Hello my name is Gabriel Martinez,")
 	print("My favorite movie is Interstellar, and it always will be. I’m also a big fan of mac and cheese.")
@@ -14,4 +13,5 @@ def main():
 
 #When we run the program this executes first
 if __name__=="__main__":
+
 	main()
