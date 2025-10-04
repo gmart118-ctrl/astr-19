bebc4c4b003b8815f32ff212b1181255c86f3d92
gmart118-ctrl <gmart118@ucsc.edu>
2025-10-03 21:07:30 -0700

    Session 2 Prompt

---
+++ floats-and-integers.py
@@ -0,0 +1,26 @@
+#This will print the sum of two 
+#floating point numbers along with data 
+#type of the resulting answer
+
+x = 9.0   #floating number
+y = 10.0  #floating number
+print("x+y = ",x+y)
+print(type(x+y))
+
+#This will print the difference
+#betweem two integers along with data
+#type of the resulting answer
+
+x = 7     #integer
+y = 3     #integer
+print("x-y =",x-y)
+print(type(x-y))
+
+#This will print the product of a
+#floating point number and an integer
+#along with data type of resulting answer
+
+x = 8.0   #floating number
+y = 5     #integer
+print("x*y =",x*y)
+print(type(x*y))
